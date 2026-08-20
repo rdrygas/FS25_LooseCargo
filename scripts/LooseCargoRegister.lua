2a4b4121a5388d3563f0a37327083b73005c642f
Robert Drygas <robert.drygas@gmail.com>
2026-08-20 22:34:37 +0200

    Update version number to 1.2.0.0

--- scripts/LooseCargoRegister.lua
+++ scripts/LooseCargoRegister.lua
@@ -1,6 +1,6 @@
 --[[
 FS25_LooseCargo
-Version 1.1.0.0
+Version 1.2.0.0
 
 Inject the specialization into all vehicle types containing FillUnit.
 Concrete vehicle filtering is performed in LooseCargo.lua by store category.
